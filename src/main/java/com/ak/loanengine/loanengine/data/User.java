47b4b000482c734a95016b60d9d80e02b293eab5
Alexandra Krauchanka <Alexandra_Krauchanka@epam.com>
2023-06-26 01:30:20 +0300

    refactoring: --surplus Setters

--- src/main/java/com/ak/loanengine/loanengine/data/User.java
+++ src/main/java/com/ak/loanengine/loanengine/data/User.java
@@ -1,7 +1,6 @@
 package com.ak.loanengine.loanengine.data;
 
 import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -15,7 +14,6 @@ import javax.persistence.Table;
  * lightweight class persisted to a USER table in a H2 DB
  */
 @Getter
-@Setter
 @Entity
 @Table(name = "USERS")
 public class User {
