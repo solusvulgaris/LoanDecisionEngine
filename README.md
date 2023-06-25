a3522a8df823bf589ec423fbea6a7856c2a4e2d5
Alexandra Krauchanka <Alexandra_Krauchanka@epam.com>
2023-06-26 00:53:51 +0300

    #5 TDD: If a person has debt then we do not approve any amount - test for this case - failed

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # LoanDecisionEngine
 Loan Decision Engine Spring Boot with a single API endpoint.
+
 The idea of the decision engine is to determine what would be the maximum sum, 
 regardless of the person requested loan amount.
+
+If a suitable loan amount is not found within the selected period, 
+the decision engine should also try to find a new suitable period.
+
+If a person has debt then we do not approve any amount. 
+If a person has no debt then we take the identifier and 
+use it for calculating person's credit score taking into account the requested input.
--- src/test/java/com/ak/loanengine/loanengine/services/LoanServiceTest.java
+++ src/test/java/com/ak/loanengine/loanengine/services/LoanServiceTest.java
@@ -1,22 +1,37 @@
 package com.ak.loanengine.loanengine.services;
 
 import com.ak.loanengine.loanengine.data.SegmentRepository;
+import com.ak.loanengine.loanengine.data.User;
 import com.ak.loanengine.loanengine.data.UserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 class LoanServiceTest {
     private final SegmentRepository segmentRepository = Mockito.mock(SegmentRepository.class);
     private final UserRepository userRepository = Mockito.mock(UserRepository.class);
     private final LoanService loanService = new LoanService(segmentRepository, userRepository);
+    private final User user = Mockito.mock(User.class);
 
     @Test
     void getDecision() {
         BigDecimal expectedLoanAmount = new BigDecimal(200);
         BigDecimal actualLoanAmount = loanService.calculateLoan("123", "200", "3");
         Assertions.assertEquals(expectedLoanAmount, actualLoanAmount);
+    }
+
+    @Test
+    void ifUserHasDebt() {
+        when(user.isDebt()).thenReturn(true);
+        when(userRepository.findByCode(anyString())).thenReturn(Optional.of(user));
+
+        BigDecimal actualLoanAmount = loanService.calculateLoan("123", "200", "3");
+        Assertions.assertEquals(0, actualLoanAmount.intValue());
     }
 }
