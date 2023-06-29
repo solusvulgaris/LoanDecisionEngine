c1fac8a5f1fcdd6f63ba28bbfed6cf4b790638ca
Alexandra Krauchanka <Alexandra_Krauchanka@epam.com>
2023-06-29 03:11:56 +0300

    #6 loan DTO to work with web

---
+++ src/main/java/com/ak/loanengine/loanengine/util/Loan.java
@@ -0,0 +1,20 @@
+package com.ak.loanengine.loanengine.util;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+@Setter
+@Getter
+public class Loan {
+    private String personalCode;
+    @Min(2000)
+    @Max(10000)
+    private int desiredLoanAmount;
+    @Min(12)
+    @Max(60)
+    private int loanPeriod;
+    private Decision decision;
+}
